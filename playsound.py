b5161c6ffb1938eb32782d4740a660d0b327c5fe
topshager <rem28777@gmail.com>
2024-11-05 00:18:42 +0200

    Update playsound.py

--- playsound.py
+++ playsound.py
@@ -4,7 +4,7 @@ import pygame
 pygame.mixer.init()
 
 # Load and play the mp3 file
-pygame.mixer.music.load("path_to_your_audio_file.mp3")
+pygame.mixer.music.load("/Users/joshuasingrew/Documents/GitHub/safe_source/pomodoro/alarm_sound/mixkit-battleship-alarm-1001.wav")
 pygame.mixer.music.play()
 
 # Keep the script running until the music stops

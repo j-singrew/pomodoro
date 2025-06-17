e0b279106807a2d5cf69f85b671a0773ef117954
topshager <rem28777@gmail.com>
2025-06-17 08:04:39 +0200

    Update pomo.h

--- pomo.h
+++ pomo.h
@@ -2,12 +2,18 @@
 #define POMO_H_
 
 #include <iostream>
+#include <string>
+#include <chrono>
+#include <thread>
+#include <iomanip>
+
 enum class SessionType
 {
     Work,
     ShortBreak,
     LongBreak
 };
+
 class  Pomodoro
 {
 private:
@@ -17,17 +23,18 @@ private:
     int CompletedWorkSessions;
     int longInterval;
     SessionType currentSession;
+    void runsession(int duration);
+    void nextSession();
+
 public:
     Pomodoro(int workdur = 25,int shortBreakDur = 5,int longbreakDur = 15,int longBreakInt = 4);
 
-    void start();
-    void runsession(int duration);
-    void nextSession();
+    void start(); 
     void pause();
     void resume();
     void stop();
 
-    SessionType getCurrentSessionType() const {return currentSession}
+    SessionType getCurrentSessionType() const {return currentSession;};
 };
 
 

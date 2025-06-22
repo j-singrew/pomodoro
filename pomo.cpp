fcac2e61e51b284a7f2cf04b5b5d804bd1b60085
topshager <rem28777@gmail.com>
2025-06-22 14:29:43 +0200

    update pomo.cpp

--- pomo.cpp
+++ pomo.cpp
@@ -11,7 +11,44 @@ int main()
     Pomodoro myPomo;
 
     std::cout <<"Welcome to your pomodoro Timer" << std::endl;
-    std::cout << "Current session type: ";
+    std::string command;
     
+    while(true){
+    std::cout << "Current session type: ";
+    switch (myPomo.getCurrentSessionType()){
+        case SessionType::Work:
+            std::cout << "Work";
+            break;
+        case SessionType::Work:
+            std::count << "Short break";
+        case SessionType::LongBreak:
+            std::cout <<"Long Break";
+            break;
+    }
+    std::cout << std::endl;
+    std::cout <<"Enter command (start,pause,resume,stop,exit): ";
+    std::cin >> command;
+
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(),'\n');
+    if (command == "start"){
+        myPomo.startSession();
+    }else if (command == "pause"){
+        myPomo.pause();
+    }else if (command == "resume"){
+        myPomo.resume();
+    }else if (command =="stop"){
+        myPomo.stop();
+    }else if (command=="exit"){
+        if (myPomo.getCurrentSessionType()){
+            std::cout <<"A session is active .Stopping it before exiting." << std::endl;
+            myPomo.stop();
+        }
+        std::cout << "Exiting Pomodoro Timer .Goodbye!" << std::endl;
+        break;
+    }else{
+        std::cout <<"Invalid command .Please try again " << std::end;
+    }
+    }
+    return 0;
 
 }

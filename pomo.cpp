4f394e6ca837494241cd6d04fccaca40f2fdce9d
topshager <rem28777@gmail.com>
2025-06-22 14:17:43 +0200

    Update pomo.cpp

--- pomo.cpp
+++ pomo.cpp
@@ -1,10 +1,17 @@
 
 #include "pomo.h"
 #include <iostream>
+#include <string>
+#include <limits>
 
 
 
 int main()
 {
+    Pomodoro myPomo;
+
+    std::cout <<"Welcome to your pomodoro Timer" << std::endl;
+    std::cout << "Current session type: ";
+    
 
 }
